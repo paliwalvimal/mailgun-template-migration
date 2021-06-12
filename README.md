0e69c7d24874d483eaa78bf8c6cc3caf7e27006f
Vimal Paliwal <vimalpaliwal.me@gmail.com>
2021-06-12 10:30:53 +0530

    optimise code and add badges

--- README.md
+++ README.md
@@ -1,11 +1,8 @@
 ## mailgun-template-migration
 
+![License](https://img.shields.io/github/license/paliwalvimal/mailgun-template-migration?style=for-the-badge) ![CodeQL](https://img.shields.io/github/workflow/status/paliwalvimal/mailgun-template-migration/codeql/main?label=CodeQL&style=for-the-badge) ![Commit](https://img.shields.io/github/last-commit/paliwalvimal/mailgun-template-migration?style=for-the-badge) ![Release](https://img.shields.io/github/v/release/paliwalvimal/mailgun-template-migration?style=for-the-badge)
+
 Migrate mailgun email templates from one domain to another
-
-## License:
-[![License: MIT](https://img.shields.io/badge/License-MIT-green.svg)](https://opensource.org/licenses/MIT)
-
-MIT License. See [Licence](LICENCE) for full details.
 
 ### Usage:
  - Set required environment variables
@@ -13,9 +10,10 @@ MIT License. See [Licence](LICENCE) for full details.
 
 ### Environment Variables:
 ```bash
-MG_OLD_MAIL_DOMAIN=''  # Domain name under which template exists
-MG_NEW_MAIL_DOMAIN=''  # Domain name to which template needs to be copied or moved to
-MG_API_KEY=''          # Mailgun API Key
+MG_BASE_URL='https://api.mailgun.net/v3'    # Mailgun API base URL
+MG_OLD_MAIL_DOMAIN='abc.com'                # Domain name under which template exists
+MG_NEW_MAIL_DOMAIN='xyz.com'                # Domain name to which template needs to be copied or moved to
+MG_API_KEY='xxxxxxxxxx'                     # Mailgun API Key
 ```
 
 ### Command:
--- mailgun-template-migration.py
+++ mailgun-template-migration.py
@@ -1,12 +1,11 @@
-import json
 import requests
 import sys
 import os
 
-MG_BASE_URL = 'https://api.eu.mailgun.net/v3'
-MG_OLD_MAIL_DOMAIN = os.environ.get('MG_OLD_MAIL_DOMAIN')
-MG_NEW_MAIL_DOMAIN = os.environ.get('MG_NEW_MAIL_DOMAIN')
-MG_API_KEY = os.environ.get('MG_API_KEY')
+MG_BASE_URL = os.environ.get('MG_BASE_URL', 'https://api.mailgun.net/v3')
+MG_OLD_MAIL_DOMAIN = os.environ.get('MG_OLD_MAIL_DOMAIN', None)
+MG_NEW_MAIL_DOMAIN = os.environ.get('MG_NEW_MAIL_DOMAIN', None)
+MG_API_KEY = os.environ.get('MG_API_KEY', None)
 
 def get_template(name):
     r = requests.get('{}/{}/templates/{}'.format(MG_BASE_URL, MG_OLD_MAIL_DOMAIN, name), auth=('api', MG_API_KEY), params={'active': 'yes'})
@@ -39,7 +38,7 @@ def get_all_templates():
             resp = r.json()
             for item in resp['items']:
                 templateNames.append(item['name'])
-            
+
             if len(resp['items']) > 0:
                 params['page'] = 'next'
                 params['p'] = resp['items'][len(resp['items']) - 1]['name']
@@ -50,34 +49,27 @@ def get_all_templates():
 
     return templateNames
 
-def copy_template():
+def migrate_template(operation):
     templateNames = get_all_templates()
-    print('Templates to be copied: {}'.format(templateNames if len(templateNames) > 0 else 'None'))
+    print('Template(s) to {}: {}'.format(operation, templateNames if len(templateNames) > 0 else 'None'))
     for templateName in templateNames:
         print('Copying template {} to domain {}'.format(templateName, MG_NEW_MAIL_DOMAIN))
         resp = get_template(templateName)
         r = create_template(templateName, resp['description'], resp['version']['template'])
-        print('Templated {} created in new domain'.format(templateName) if r else 'Failed to create template {} in new domain'.format(templateName))
-
-def move_template():
-    templateNames = get_all_templates()
-    print('Templates to be moved: {}'.format(templateNames if len(templateNames) > 0 else 'None'))
-    for templateName in templateNames:
-        print('Copying template {} to domain {}'.format(templateName, MG_NEW_MAIL_DOMAIN))
-        resp = get_template(templateName)
-        r = create_template(templateName, resp['description'], resp['version']['template'])
-        print('Templated {} created in new domain'.format(templateName) if r else 'Failed to create template {} in new domain'.format(templateName))
-        r = delete_template(templateName)
-        print('Templated {} deleted from old domain'.format(templateName) if r else 'Failed to delete template {} from old domain'.format(templateName))
+        print('Template {} created in new domain'.format(templateName) if r else 'Failed to create template {} in new domain'.format(templateName))
+        if operation == 'move':
+            r = delete_template(templateName)
+            print('Template {} deleted from old domain'.format(templateName) if r else 'Failed to delete template {} from old domain'.format(templateName))
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print('Argument not found. Usage: python3 mailgun-template-migration.py {copy|move}')
+        print('Argument not found. Usage: python3 mailgun-template-migration.py [copy | move]')
         exit(1)
-    elif sys.argv[1] == 'copy':
-        copy_template()
-    elif sys.argv[1] == 'move':
-        move_template()
+    elif MG_OLD_MAIL_DOMAIN is None or MG_NEW_MAIL_DOMAIN is None or MG_API_KEY is None:
+        print('Environment variables MG_OLD_MAIL_DOMAIN, MG_NEW_MAIL_DOMAIN and MG_API_KEY are required. Current values:\nMG_OLD_MAIL_DOMAIN: {}\nMG_NEW_MAIL_DOMAIN: {}\nMG_API_KEY: {}'.format(MG_OLD_MAIL_DOMAIN, MG_NEW_MAIL_DOMAIN, MG_API_KEY))
+        exit(1)
+    elif sys.argv[1] != 'copy' and sys.argv[1] != 'move':
+        print('Invalid argument. Usage: python3 mailgun-template-migration.py [copy | move]')
+        exit(1)
     else:
-        print('Invalid argument. Usage: python3 mailgun-template-migration.py {copy|move}')
-        exit(1)
+        migrate_template(sys.argv[1])
